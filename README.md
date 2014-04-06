ca4c8c5f0bc249383b18b565b212fd977a6efbcd
matt <mdfreezer@gmail.com>
2014-04-06 18:19:32 +0200

    - added necessary files to start project

---
+++ README.md
@@ -0,0 +1,9 @@
+## Array.rotate();
+
+===
+
+Please note, this is only a first attempt.
+More advanced examples will follow.
+
+### What it does?
+It rotates a two dimensional Array.
---
+++ array.rotate.js
@@ -0,0 +1,37 @@
+
+Array.prototype.rotate = function ( dir ) {
+    var that = this,
+        direction = dir !== "ccw",
+        flat = [],
+        columns = that[0].length,
+        tmp = [];
+
+    /* check if used array has proper structure */
+    if ( !(that.length) || !columns ) {
+        console.error("incompatible array lengths");
+        return false;
+    }
+
+    /* flatten array entries */
+    for ( var i = 0, l = that.length; i < l; i++ ) {
+        for ( var j = 0, m = that[i].length; j < m; j++ ) {
+            if (direction) {
+                flat.push(that[i][j]);
+            } else {
+                flat.unshift(that[i][j]);
+            }
+        }
+    }
+
+    /* set up modified array structure */
+    for ( var k = 0, n = flat.length; k < n; k++ ) {
+        var index = k % columns;
+        if ( !tmp[index] ) {
+            tmp[index] = [flat[k]];
+        } else {
+            tmp[index].unshift(flat[k]);
+        }
+    }
+
+    return tmp;
+};
